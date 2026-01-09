99fc70f087c4700df93a79843cb21fb6d1cdbda7
damipic100-sys <damipic100@gmail.com>
2026-01-08 21:18:52 -0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ btn.onclick = async () => {
   ladderEl.innerHTML = "<li>Cargando...</li>";
 
   try {
-    const res = await fetch("/data/ladder.json");
+    const res = await fetch("/ladder.json");
     const data = await res.json();
 
     ladderEl.innerHTML = "";
